2c52538108100d87d0aed8fc30f17503bb4ca9ba
Ayushchaubey-114 <ayushchaubey966@gmail.com>
2025-11-29 13:17:27 +0530

    Create whatapp.py

---
+++ whatapp.py
@@ -0,0 +1,9 @@
+import pywhatkit
+pywhatkit.sendwhatmsg_instantly(
+    "+917970984389",
+    "Hi, This message is automated by Python")
+pywhatkit.sendwhatmsg(
+    "+917000905661",
+    "Hi, This message is automated by Python on set time",
+    11, 45
+)  
