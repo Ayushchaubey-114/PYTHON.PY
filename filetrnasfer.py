e080414a0ca2d855d80e51bba295e92584230074
Ayushchaubey-114 <ayushchaubey966@gmail.com>
2025-11-29 13:21:23 +0530

    Create filetrnasfer.py

---
+++ filetrnasfer.py
@@ -0,0 +1,9 @@
+import http
+import socketserver
+PORT = 8000
+
+handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), handler) as https:
+    print("Http server is running on PORT ", PORT)
+    #http.serve_forever()
